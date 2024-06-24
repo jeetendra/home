f7f4913f6c95da50bef7f859467ae2b9b99c4bb0
Jeetendra Singh <chauhanjeetendra@gmail.com>
2024-06-25 00:16:53 +0530

    link post title to page

--- nextjs-app/src/app/posts/page.tsx
+++ nextjs-app/src/app/posts/page.tsx
@@ -1,17 +1,20 @@
+import Link from "next/link";
 
 export default async () => {
-
-    const posts = await fetch('https://dummyjson.com/posts?select=title', { cache: "force-cache" }).then(res => res.json());
-    console.log(posts);
-    return (
-        <div>
-            <h2>Posts:</h2>
-            <ul>
-                {
-                    posts.posts.map(item => <li key={item.id}>{item.title}</li>)
-                }   
-            </ul>
-                  
-        </div>
-    );
-}
+  const posts = await fetch("https://dummyjson.com/posts?select=title", {
+    cache: "force-cache",
+  }).then((res) => res.json());
+  console.log(posts);
+  return (
+    <div>
+      <h2>Posts:</h2>
+      <ul>
+        {posts.posts.map((item) => (
+          <li key={item.id}>
+            <Link href={`posts/${item.id}`}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
