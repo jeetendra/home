277958aa13f761e92c70ad6e5bec2a320b88bd57
Jeetendra Singh <chauhanjeetendra@gmail.com>
2024-07-04 22:47:28 +0530

    added route handler

---
+++ nextjs-app/src/app/api/route.ts
@@ -0,0 +1,22 @@
+import { cookies } from 'next/headers'
+
+export async function GET() {
+    const res = await fetch('https://dummyjson.com/posts');
+    const data = await res.json()
+     
+    return Response.json({ data })
+} 
+
+
+
+export async function POST() {
+    const cookieStore = cookies()
+    let token = Number(cookieStore.get('token')?.value) || 0;
+    
+    token++;
+
+    return new Response('Hello, Next.js!', {
+        status: 200,
+        headers: { 'Set-Cookie': `token=${token}` },
+      })
+} 
